13dd6b5429a7fe072e5c72548e029245f1e72c6e
jacobiajohnson <jacobia@workos.com>
2024-10-01 14:43:09 -0400

    Change name

--- sst.config.ts
+++ sst.config.ts
@@ -3,7 +3,7 @@
 export default $config({
   app(input) {
     return {
-      name: "monorepo-template",
+      name: "lifeos",
       removal: input?.stage === "production" ? "retain" : "remove",
       home: "aws",
     };
